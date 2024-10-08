c5e2f52e4b57f1db954953d00d072c8af0749033
Tony <antoniofstefan@outlook.com>
2024-10-08 18:16:34 -0400

    added basic comm + changed config format
    config format went from
    self[att_name] = value
    to
    self._config[att_name] = {
        ["value"] = value,
        ["min"] = min,
        ["max"] = max,
        ["parent_script"] = module
    }

---
+++ server/init.server.lua
@@ -0,0 +1,3 @@
+require(script.Parent.Configify)
+
+local SWORD_DAMAGE = _G.Cfg:Set("SWORD_DMG", 10, 0, 100)
--- src/init.lua
+++ src/init.lua
@@ -40,12 +40,17 @@ local KEYBIND = script:GetAttribute("ToggleKeybind") or "RightAlt"
 
 local cas = game:GetService("ContextActionService")
 local runservice = game:GetService("RunService")
+local players = game:GetService("Players")
 
 local configify = {}
 configify.__index = configify
 
-local tab_cache = {}
+local tab_cache = nil
 local is_client = runservice:IsClient()
+
+if is_client then
+    tab_cache = {}
+end
 type att_type = string | boolean | number | UDim | UDim2 | BrickColor | Color3 | Vector2 | Vector3 | CFrame | NumberSequence | ColorSequence | NumberRange | Rect | Font
 
 local function get_tab_name() -- get name of script this is being called in
@@ -67,29 +72,47 @@ end
 function configify.new()
     local self = setmetatable({}, configify)
 
-    if _G.Configify then
-        warn("Configify already exists!")
-        return
+    if is_client then
+        self._UI = nil
+        self.UI_Changed = nil
+
+        if script:FindFirstChild("Comm") then
+            self._comm = script.Comm
+            self:_ListenForComm()
+        else
+            local c 
+            c = script.ChildAdded:Connect(function(child)
+                if not (child.Name == "Comm") then
+                    return
+                end
+
+                c:Disconnect()
+                c = nil
+
+                self._comm = child
+                self:_ListenForComm()
+            end)
+        end
+    else
+        self._comm = Instance.new("RemoteEvent")
+        self._comm.Name = "Comm"
+        self._comm.Parent = script
+        self:_ListenForComm()
     end
 
-    if not is_client then
-        warn("Only client use of Configify is currently supported, please use on client")
-        return
-    end
-
-    self._UI = nil
-    self.UI_Changed = nil
+    self._config = {}
     self:_Init()
 
     return self
 end
 
-function configify:_Init()
+function configify:_CreateUI()
     self._UI = require(script.UI).new()
 
     self._UI.UIChanged:Connect(function(att_name, att_value, module)
         module:SetAttribute(att_name, att_value)
-        self[att_name] = att_value
+
+        self._config[att_name].value = att_value
     end)
 
     local function toggle(action_name, input_state, input_obj)
@@ -106,6 +129,24 @@ function configify:_Init()
         cas:UnbindAction("ToggleConfigify")
         cas:BindAction("ToggleConfigify", toggle, false, Enum.KeyCode[script:GetAttribute("ToggleKeybind")])
     end)
+end
+
+function configify:_Init()
+    if is_client then
+        self:_CreateUI()
+    else
+        local function initialize_existing_config(player)
+            self._comm:FireClient(player, "all", self._config)
+        end
+        
+        for i, v in players:GetPlayers() do
+            initialize_existing_config(v)
+        end
+
+        players.PlayerAdded:Connect(function(player)
+            initialize_existing_config(player)    
+        end)
+    end
 
     _G.Configify = self
     _G.Cfg = self -- Alias
@@ -115,27 +156,52 @@ function configify:Set(att_name: string, initial: att_type, min: att_type, max: 
     local module = get_module(3)
     local initial_type = type(initial)
 
-    if tab_cache[module] == nil then
-        self._UI:AddTab(module)
-        tab_cache[module] = true
-    end
+    if is_client then
+        if tab_cache[module] == nil then
+            self._UI:AddTab(module)
+            tab_cache[module] = true
+        end
 
-    self._UI:AddConfig(att_name, initial, min, max, module)
+        self._UI:AddConfig(att_name, initial, min, max, module)
+    else
+        --TODO: Change this to fire specific clients based on a whitelist/group rank
+        self._comm:FireAllClients(att_name, initial, min, max, module)
+    end
 
     module:SetAttribute(att_name, initial)
     module:GetAttributeChangedSignal(att_name):Connect(function()
         local v = module:GetAttribute(att_name)
-        self[att_name] = v
+        self._config[att_name].value = v
     end)
 
-    self[att_name] = initial
+    self._config[att_name] = {
+        ["value"] = initial,
+        ["min"] = min,
+        ["max"] = max,
+        ["parent_script"] = module
+    }
 
     return function()
         if initial_type == "number" then
-            return tonumber(self[att_name])
+            return tonumber(self._config[att_name].value)
         else
-            return self[att_name]
+            return self._config[att_name].value
         end
+    end
+end
+
+function configify:_ListenForComm()
+    if is_client then
+        -- when the client starts listening, it has to request the current server config
+        -- this is because the server is too fast and couldve set config before the client event connected to the remote
+
+        self._comm.OnClientEvent:Connect(function(att_name, initial, min, max, module)
+            print(`module: {module}, new config: {att_name}, initial value: {initial}, min: {min}, max: {max}`)
+        end)
+    else
+        self._comm.OnServerEvent:Connect(function()
+            print("Server received!")
+        end)
     end
 end
 
