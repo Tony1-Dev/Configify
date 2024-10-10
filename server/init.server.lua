029506715fc0152ef75919a3d54c4308951bb9eb
Tony <antoniofstefan@outlook.com>
2024-10-10 10:56:12 -0400

    Test

--- server/init.server.lua
+++ server/init.server.lua
@@ -4,6 +4,8 @@ local SWORD_DAMAGE = _G.Cfg:Set("SWORD_DMG", 10, 0, 100)
 local BOOL = _G.Cfg:Set("BOOL", false)
 local SERVER_NAME = _G.Cfg:Set("SERVER_NAME", "N/A")
 
+print("Test")
+
 while true do
     task.wait(10)
     print(SWORD_DAMAGE())
